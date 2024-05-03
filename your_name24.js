809256e42fdeae964e180e7e6ec7230f46c77142
Peterfiorica <166462544+Peterfiorica@users.noreply.github.com>
2024-05-02 22:52:59 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -11,3 +11,23 @@ var marker = L.marker([40.57440999440993, -74.11546985003544]).addTo(map);
 
 // Add a popup to the marker
 marker.bindPopup("<b>La Strada Italian Restaurant</b><br>This is a Leaflet map.").openPopup();
+
+// Load the GeoJSON line file
+fetch('https://https://peterfiorica.github.io/Peterfiorica/PeterFiorica_daytimeroute.geojson)
+    .then(response => response.json())
+    .then(geojson => {
+        // Customize the style of the line
+        var lineStyle = {
+            color: 'blue', // Change color as needed
+            weight: 5, // Change weight as needed
+            opacity: 0.9 // Change opacity as needed
+        };
+
+        // Add the GeoJSON line to the map
+        L.geoJSON(geojson, {
+            style: lineStyle
+        }).addTo(map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON file:', error);
+    });
