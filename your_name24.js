ffde8076b24a281dac84547ead5c1438d2966349
Peterfiorica <166462544+Peterfiorica@users.noreply.github.com>
2024-05-07 19:14:54 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -17,7 +17,7 @@ var marker3 = L.marker([40.57613950267241, -74.12379090325251]).addTo(map);
 marker3.bindPopup("<b>FDNY Engine 165 Ladder 85</b><br>This firehouse is always the starting point for my walk.<br><img src='https://raw.githubusercontent.com/Peterfiorica/Peterfiorica/main/images/FDNY.jpeg' alt='Firehouse Image' style='max-width: 100px;'>").openPopup();
 
 var marker4 = L.marker([40.58096673060801, -74.12403125025611]).addTo(map);
-marker4.bindPopup("<b>Walker Pond</b><br>Nice pond that I always pass by.<br><img src='https://https://github.com/Peterfiorica/Peterfiorica/blob/main/images/walker-pond.jpg' alt='Restaurant Image' style='max-width: 100px;'>").openPopup();
+marker4.bindPopup("<b>Walker Pond</b><br>Nice pond that I always pass by.<br><img src='https://github.com/Peterfiorica/Peterfiorica/raw/main/images/walker-pond.jpg' alt='Walker Pond Image' style='max-width: 100px;'>").openPopup();
 
 var marker5 = L.marker([40.57591118383676, -74.12757071055918]).addTo(map);
 marker5.bindPopup("<b>Amazing Games TCG</b><br>Cool game store that is right by my walk.").openPopup();
@@ -32,7 +32,7 @@ var marker8 = L.marker([40.57016431405762, -74.10955103578445]).addTo(map);
 marker8.bindPopup("<b>Teriyaki One</b><br>Delicious restaurant.<br><img src='https://github.com/Peterfiorica/Peterfiorica/raw/main/images/TerryakiOne.jpeg' alt='Teriyaki One Image' style='max-width: 200px;height:auto;'>").openPopup();
 
 var marker9 = L.marker([40.574077461086425, -74.11662680325256]).addTo(map);
-marker9.bindPopup("<b>New Dorp Train Station</b><br>I normally take this train home after my walk.<br><img src='https://github.com/Peterfiorica/Peterfiorica/raw/main/images/NewDorpTrain.jpeg' alt='New Dorp Train Station Image' style='max-width: 200px;height:auto;'>
+marker9.bindPopup("<b>New Dorp Train Station</b><br>I normally take this train home after my walk.<br><img src='https://github.com/Peterfiorica/Peterfiorica/raw/main/images/NewDorpTrain.jpeg' alt='New Dorp Train Station Image' style='max-width: 200px;height:auto;'>").openPopup();
 
 // Load the GeoJSON line file for day time route
 fetch('https://peterfiorica.github.io/Peterfiorica/PeterFiorica_daytimeroute.geojson')
