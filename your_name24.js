28501a0a2a52f3c8fc4941c0769fb3b384a115b4
Peterfiorica <166462544+Peterfiorica@users.noreply.github.com>
2024-05-07 11:35:24 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -12,45 +12,8 @@ var marker = L.marker([40.57440999440993, -74.11546985003544]).addTo(map);
 // Add a popup to the marker
 marker.bindPopup("<b>La Strada Italian Restaurant</b><br>This is a Leaflet map.").openPopup();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Load the GeoJSON line file
-fetch('https://https://peterfiorica.github.io/Peterfiorica/PeterFiorica_daytimeroute.geojson)
+// Load the GeoJSON line file for day time route
+fetch('https://peterfiorica.github.io/Peterfiorica/PeterFiorica_daytimeroute.geojson')
     .then(response => response.json())
     .then(geojson => {
         // Customize the style of the line
@@ -69,23 +32,22 @@ fetch('https://https://peterfiorica.github.io/Peterfiorica/PeterFiorica_daytimer
         console.error('Error loading GeoJSON file:', error);
     });
 
-
-    // Load the GeoJSON line file
+// Load the GeoJSON line file for night time route
 fetch('https://Peterfiorica.github.io/Peterfiorica/PeterFiorica_nightimeroute.geojson')
-.then(response => response.json())
-.then(geojson => {
-    // Customize the style of the line
-    var lineStyle = {
-        color: 'blue', // Change color as needed
-        weight: 5, // Change weight as needed
-        opacity: 0.7 // Change opacity as needed
-    };
+    .then(response => response.json())
+    .then(geojson => {
+        // Customize the style of the line
+        var lineStyle = {
+            color: 'blue', // Change color as needed
+            weight: 5, // Change weight as needed
+            opacity: 0.7 // Change opacity as needed
+        };
 
-    // Add the GeoJSON line to the map
-    L.geoJSON(geojson, {
-        style: lineStyle
-    }).addTo(map);
-})
-.catch(error => {
-    console.error('Error loading GeoJSON file:', error);
-});
+        // Add the GeoJSON line to the map
+        L.geoJSON(geojson, {
+            style: lineStyle
+        }).addTo(map);
+    })
+    .catch(error => {
+        console.error('Error loading GeoJSON file:', error);
+    });
